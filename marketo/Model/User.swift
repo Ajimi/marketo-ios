350951df3be17e66cd8aaa81024609230a0af613
Moncef Guettat <moncefguettat@moncefs-mac.local>
2018-11-10 21:04:22 +0100

    add User Model

---
+++ marketo/Model/User.swift
@@ -0,0 +1,24 @@
+//
+//  User.swift
+//  marketo
+//
+//  Created by Moncef Guettat on 11/10/18.
+//  Copyright © 2018 selim ajimi. All rights reserved.
+//
+
+import Foundation
+
+class User{
+    
+    var fullName : String
+    var username : String
+    var password : String?
+    var email : String
+    
+    init(fullName : String, username : String,password : String?, email:String) {
+        self.fullName = fullName
+        self.username = username
+        self.password = password
+        self.email = email
+    }
+}
