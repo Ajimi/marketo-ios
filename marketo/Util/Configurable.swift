24189129a48b0cf8d3130880dbe490e58538eda0
Moncef Guettat <moncef.guettat.1@esprit.tn>
2018-12-01 12:57:59 +0100

    add Configurable Protocol

---
+++ marketo/Util/Configurable.swift
@@ -0,0 +1,15 @@
+//
+//  Configurable.swift
+//  marketo
+//
+//  Created by Moncef Guettat on 12/1/18.
+//  Copyright © 2018 selim ajimi. All rights reserved.
+//
+
+import Foundation
+
+protocol Configurable {
+    
+    func configure(with content: Any?)
+    
+}
