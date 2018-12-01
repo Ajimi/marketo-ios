27a3343d53f512fd6d46754439179a91759d2735
Moncef Guettat <moncef.guettat.1@esprit.tn>
2018-12-01 12:56:59 +0100

    Update Category UI
    add Category Collection View

---
+++ marketo/UI/Category/widget/CategoryCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CategoryCollectionViewCell.swift
+//  marketo
+//
+//  Created by Moncef Guettat on 12/1/18.
+//  Copyright © 2018 selim ajimi. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell,Configurable{
+    
+    @IBOutlet weak var image : UIImageView!
+    @IBOutlet weak var label : UILabel!
+    
+    func configure(with content: Any?) {
+        
+    }
+}
