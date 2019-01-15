bc046aac5bb05e670dd533a372f4e0fc0366f407
Moncef Guettat <moncef.guettat.1@esprit.tn>
2019-01-15 13:55:21 +0100

    Update Featured UI
    - update Font
    - update Discounted Product Cell

--- marketo/UI/Featured/widget/DiscountedProductCollectionViewCell.swift
+++ marketo/UI/Featured/widget/DiscountedProductCollectionViewCell.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class DiscountedProductCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var productName : UILabel!
+    @IBOutlet weak var marketName : UILabel!
+    @IBOutlet weak var discount : UILabel!
     
     func configure(with product: Product){
-        // TODO ADD IMAGE
+
+        productName.text = product.name
+        marketName.text = getBestDiscountPrice(product: product).market?.text
+        discount.text = (getBestDiscountPrice(product: product).discount! * Float(100)).clean + "%"
         image.kf.setImage(
             with: URL(string: product.image!),
             placeholder: UIImage(named: "logo"),
@@ -22,4 +28,17 @@ class DiscountedProductCollectionViewCell: UICollectionViewCell {
                 .cacheOriginalImage
             ])
     }
+}
+
+extension DiscountedProductCollectionViewCell{
+    
+    func getBestDiscountPrice(product : Product) -> Price {
+        
+        return (product.prices?.max(by: { (price1, price2) -> Bool in
+            (price2.discount?.isLess(than: price1.discount ?? 0)) ?? false
+        }))!
+        
+    }
+    
+    
 }
--- marketo/UI/Widget/Extension.swift
+++ marketo/UI/Widget/Extension.swift
@@ -16,3 +16,9 @@ extension UIImageView {
         self.clipsToBounds = true
     }
 }
+
+extension Float {
+    var clean: String {
+        return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self)
+    }
+}
