7e4104d9be1852f80c036cf26addf9246f0d50b5
Selim Ajimi <sloumma.21@gmail.com>
2019-01-17 14:55:10 +0300

    Add Delegates

---
+++ marketo/UI/More/delegates/SignInUserTableViewCellDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  SignInUserTableViewCellDelegate.swift
+//  marketo
+//
+//  Created by Admin on 1/17/19.
+//  Copyright © 2019 selim ajimi. All rights reserved.
+//
+
+import Foundation
---
+++ marketo/UI/More/delegates/UserNameTableViewDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  UserNameTableViewDelegate.swift
+//  marketo
+//
+//  Created by Admin on 1/17/19.
+//  Copyright © 2019 selim ajimi. All rights reserved.
+//
+
+import Foundation
